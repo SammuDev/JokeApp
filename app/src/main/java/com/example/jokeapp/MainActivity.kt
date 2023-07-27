e277d9d251126586e4739d92094c9d79e832fa0a
SammuDev <samucas878@gmail.com>
2023-07-27 09:48:03 -0300

    doc: Fazendo a ligação dos 'Fragments' na 'MainActivity'

--- app/src/main/java/com/example/jokeapp/MainActivity.kt
+++ app/src/main/java/com/example/jokeapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.jokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+
+        val navigationController = findNavController(R.id.navigationFragment_content_main)
+
+        navigationView.setupWithNavController(navigationController)
     }
 }
